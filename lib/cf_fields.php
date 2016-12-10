db2d9b489b797c3abb24835b02628b319508cce6
Jonathan Waldstein <Jpwaldstein@gmail.com>
2016-12-09 20:07:53 -0500

    content repeater blocks

--- lib/cf_fields.php
+++ lib/cf_fields.php
@@ -15,8 +15,10 @@ Container::make( 'post_meta', 'Page Builder' )//PAGE BUILDER FIELDS
             Field::make("separator", "section_settings"),
             Field::make('checkbox', 'full_width_section')->set_width(20),
             Field::make('checkbox', 'content_contained')->set_width(20),
-            Field::make('text', 'section_class')->set_width(30),
-            Field::make('image', 'section_background_image')->set_value_type( 'url' )->set_width(30),
+            Field::make('checkbox', 'mobile_center_text')->set_width(20),
+            Field::make('checkbox', 'mobile_reverse_columns')->set_width(20),
+            Field::make('text', 'section_class')->set_width(50),
+            Field::make('image', 'section_background_image')->set_value_type( 'url' )->set_width(50),
             Field::make("separator", "section_heading"),
             Field::make('text', 'section_heading'),
             Field::make("select", "heading_tag", "Heading Tag")
@@ -47,7 +49,36 @@ Container::make( 'post_meta', 'Page Builder' )//PAGE BUILDER FIELDS
              ))   
             ->add_fields( 'Column', array(
                 Field::make('text', 'column_class'),
-                Field::make( 'rich_text', 'column_content' )->set_width(50)
+                Field::make( 'complex', 'column_content' )
+                 ->setup_labels( array(
+                    'plural_name' => 'Content',
+                    'singular_name' => 'Content'
+                 ))
+                 ->add_fields( 'Content Block', array(
+                    Field::make("select", "content_type")
+                     ->add_options(array(
+                        'text' => 'Text',
+                        'image' => 'Image',
+                    )),
+                      Field::make( 'rich_text', 'content_text' )
+                        ->set_conditional_logic(array(
+                            'relation' => 'OR',
+                             array(
+                                'field' => 'content_type',
+                                'value' => 'text', 
+                                'compare' => '=', 
+                            ),
+                        )),
+                    Field::make( 'image', 'content_image' )->set_value_type( 'url' )
+                        ->set_conditional_logic(array(
+                            'relation' => 'OR',
+                             array(
+                                'field' => 'content_type',
+                                'value' => 'image', 
+                                'compare' => '=', 
+                            ),
+                        )),
+                 ))  
             ))
         )),
 )); 
--- lib/cf_view.php
+++ lib/cf_view.php
@@ -1,7 +1,7 @@
 <?php
 function carbon_print_layout($layout) {//view fields
 	?>
-	<div class="section <?= $layout['full_width_section'] ? 'container-fluid' : 'container'; ?> <?= $layout['content_contained'] ? 'content-contained' : ''; ?> <?= $layout['section_class']; ?>" style="<?=bg_image($layout['section_background_image'])?>">
+	<div class="section <?= $layout['full_width_section'] ? 'container-fluid' : 'container'; ?> <?= $layout['content_contained'] ? 'content-contained' : null; ?>  <?= $layout['mobile_center_text'] ? 'mobile-center-text' : null; ?> <?= $layout['mobile_reverse_columns'] ? 'mobile-reverse-columns' : null; ?> <?= $layout['section_class']; ?>" style="<?=bg_image($layout['section_background_image'])?>">
 		<?php if ( $layout['_type'] === '_dynamic_section' ): ?>
 				<?php if ( $layout['section_heading'] ): ?>
 					<div class="row row-heading">
@@ -23,7 +23,17 @@ function carbon_print_layout($layout) {//view fields
 							}
 							?>
 						<div class="<?= $max_columns === 5 ? 'col-sm-2 ' : ''; ?> col-sm-<?=$col_class;?> <?= $column['column_class']; ?>">
-							<?= carbon_parse_shortcodes($column['column_content']); ?>
+							<?php $content = $column['column_content']; ?>
+							<?php if (!empty($content)): ?>
+								<?php foreach($content as $content_block ): ?>
+									<?php if ($content_block['content_type'] === 'text'): ?>
+										<?= carbon_parse_shortcodes($content_block['content_text']); ?>
+									<?php endif; ?>
+									<?php if ($content_block['content_type'] === 'image'): ?>
+										<img src="<?= $content_block['content_image']; ?>" alt=""/>
+									<?php endif; ?>
+								<?php endforeach; ?>
+							<?php endif; ?>
 						</div>
 					<?php endforeach; ?>
 				<?php endif; ?>
